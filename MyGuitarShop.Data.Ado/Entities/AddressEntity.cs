2009d85314f40fc9a6de400d6db744c5ac224a10
JacobSoleil <jacob.soleil@oit.edu>
2025-11-01 14:17:12 -0700

    Created AddressEntity

---
+++ MyGuitarShop.Data.Ado/Entities/AddressEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyGuitarShop.Data.Ado.Entities
+{
+    internal class AddressEntity
+    {
+        public required int AddressID { get; set; }
+
+        public int? CustomerID { get; set; }
+
+        [MaxLength(60)]
+        public required string Line1 { get; set; }
+
+        public string? Line2 { get; set; }
+
+        [MaxLength(40)]
+        public required string City { get; set; }
+
+        [MaxLength(2)]
+        public required string State { get; set; }
+
+        [MaxLength(10)]
+        public required string ZipCode { get; set; }
+
+        [MaxLength(12)]
+        public required string Phone { get; set; }
+
+        public required int Disabled { get; set; }
+    }
+}
