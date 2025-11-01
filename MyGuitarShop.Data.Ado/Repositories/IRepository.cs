19a652bd75908756b17135c96e55f6657577ec22
JacobSoleil <jacob.soleil@oit.edu>
2025-11-01 14:23:14 -0700

    Created IRepository

---
+++ MyGuitarShop.Data.Ado/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuitarShop.Data.Ado.Repositories
+{
+    internal interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> FindByIdAsync(int id);
+        Task<int> InsertAsync(T entity);
+        Task<int> UpdateAsync(int id, T entity);
+        Task<int> DeleteAsync(int id);
+    }
+}
