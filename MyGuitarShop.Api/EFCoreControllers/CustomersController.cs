4d60d1886907723a79407a8ab56114d1452eb4bc
JacobSoleil <jacob.soleil@oit.edu>
2025-11-14 11:47:37 -0800

    Added BaseController and all children

---
+++ MyGuitarShop.Api/EFCoreControllers/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGuitarShop.Api.Abstract;
+using MyGuitarShop.Common.DTOs;
+using MyGuitarShop.Data.EFCore.Entities;
+using MyGuitarShop.Data.EFCore.Repositories;
+
+namespace MyGuitarShop.Api.EFCoreControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersEFCoreController(
+        CustomerRepository repository,
+        ILogger<CustomersEFCoreController> logger)
+        : BaseController<CustomerDto, Customer>(repository, logger)
+    {
+    }
+}
---
+++ MyGuitarShop.Api/EFCoreControllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGuitarShop.Api.Abstract;
+using MyGuitarShop.Common.DTOs;
+using MyGuitarShop.Data.EFCore.Entities;
+using MyGuitarShop.Data.EFCore.Repositories;
+
+namespace MyGuitarShop.Api.EFCoreControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersEFCoreController(
+        OrderRepository repository,
+        ILogger<OrdersEFCoreController> logger)
+        : BaseController<OrderDto, Order>(repository, logger)
+    {
+    }
+}
