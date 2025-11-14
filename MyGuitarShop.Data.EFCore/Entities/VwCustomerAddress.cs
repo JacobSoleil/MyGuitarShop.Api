dab16dee9c079dde29ae8af1bdd602aa884e83c7
JacobSoleil <jacob.soleil@oit.edu>
2025-11-14 08:39:08 -0800

    Created entity frameworks

---
+++ MyGuitarShop.Data.EFCore/Entities/VwCustomerAddress.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyGuitarShop.Data.EFCore.Entities;
+
+public partial class VwCustomerAddress
+{
+    public int CustomerId { get; set; }
+
+    public string EmailAddress { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string BillLine1 { get; set; } = null!;
+
+    public string? BillLine2 { get; set; }
+
+    public string BillCity { get; set; } = null!;
+
+    public string BillState { get; set; } = null!;
+
+    public string BillZip { get; set; } = null!;
+
+    public string ShipLine1 { get; set; } = null!;
+
+    public string? ShipLine2 { get; set; }
+
+    public string ShipCity { get; set; } = null!;
+
+    public string ShipState { get; set; } = null!;
+
+    public string ShipZip { get; set; } = null!;
+}
